44df032df3ede36ee8387a4777745376fd23853c
mateomarcos <marcosmateoiae@gmail.com>
2024-05-09 13:40:21 -0300

    First commit

--- calra_example/calra_example_stack.py
+++ calra_example/calra_example_stack.py
@@ -1,8 +1,12 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_iam as iam,
+    aws_lambda as _lambda,
+    aws_lambda_python_alpha as _lambda_python,
+    aws_apigateway as apigateway,
 )
+from calra_cdk import resource_builder
 from constructs import Construct
 
 class CalraExampleStack(Stack):
@@ -10,10 +14,34 @@ class CalraExampleStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        builder = resource_builder.class_name()
+        lambda_path = 'lambdas'
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CalraExampleQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        restapi = apigateway.RestApi(
+            self, 'calra-RestApi',
+            rest_api_name= 'calra-restApi')
+        root_resource = restapi.root
+
+        layer = _lambda_python.PythonLayerVersion(
+            self, "calra-lambda",
+            entry="./layers",
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_10, _lambda.Runtime.PYTHON_3_11]
+        )
+        builder.add_common_layer(layer)
+
+        default_role = iam.Role(
+            self, "calra-role",
+            assumed_by= iam.ServicePrincipal('lambda.amazonaws.com'),
+            managed_policies= [
+                iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AWSLambdaBasicExecutionRole')            
+            ]
+        )
+        builder.set_default_role(default_role)
+
+        builder.set_default_timeout(Duration.seconds(60))
+        builder.set_default_runtime(_lambda.Runtime.PYTHON_3_10)
+        builder.add_common_environment("URL", "http://example.com")
+        builder.add_custom_environment("DB_NAME_CATS", "cats")
+        builder.add_custom_environment("DB_NAME_DOGS", "dogs")
+
+        builder.create_lbd_rest_stack(self, root_resource, lambda_path, print_tree=True)
